68c03066fc88f5d7e21f84f785baa01ec581f9a0
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2022-04-04 13:14:19 +0900

    message

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+import shutil
+import os
+from uuid import uuid4
+
 import git
 
+work_dir = f"/tmp/test_git_python/{str(uuid4())}"
+repo_dir = f"{work_dir}/repo"
+sample_dir = f"{work_dir}/repo/sample"
+repo_url = "git@github.com:ng3rdstmadgke/test_git_python.git"
+print(f"work_dir: {work_dir}")
+print(f"repo_dir: {repo_dir}")
+print(f"repo_url: {repo_url}")
+os.makedirs(work_dir)
+
+git.Git().clone(repo_url, repo_dir)
+repo = git.Repo(repo_dir)
+
+shutil.rmtree(sample_dir, ignore_errors=True)
+shutil.copytree("./sample", sample_dir)
+shutil.copyfile("./main.py", f"{repo_dir}/main.py")
+
+repo.git.add(all=True)
+
+repo.index.commit("message")
+
+repo.remote("origin").push()
