18dac61202d4384bc7c39f94ed3535f166874ef0
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2022-04-04 13:40:13 +0900

    message

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+from typing import List
 import shutil
 import os
 from uuid import uuid4
 
 import git
+import yaml
+from pydantic import BaseModel
+
+class SkyportDeviceSchema(BaseModel):
+    name: str
+    ip_addr: str
+
+class SkyportSchema(BaseModel):
+    ssid: str
+    devices: List[SkyportDeviceSchema]
+
+
+tmp = {}
+tmp["ssid"] = "skyport01"
+tmp["devices"] = []
+for i in range(0, 10):
+    tmp["devices"].append(
+        {
+            "name": f"node{i}",
+            "ip_addr": f"192.168.50.{i}"
+        }
+    )
+
+schema = SkyportSchema.parse_obj(tmp)
 
 work_dir = f"/tmp/test_git_python/{str(uuid4())}"
 repo_dir = f"{work_dir}/repo"
@@ -19,6 +44,10 @@ repo = git.Repo(repo_dir)
 shutil.rmtree(sample_dir, ignore_errors=True)
 shutil.copytree("./sample", sample_dir)
 shutil.copyfile("./main.py", f"{repo_dir}/main.py")
+shutil.copyfile("./requirements.txt", f"{repo_dir}/requirements.txt")
+with open(f"{repo_dir}/vars.yml", "w") as writer:
+    writer.write(yaml.dump(schema.dict()))
+
 
 repo.git.add(all=True)
 
